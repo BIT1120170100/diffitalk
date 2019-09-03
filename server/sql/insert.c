926c1da62d181c919e2afc0a101d4dd80602c19f
yuyuyuolalalala <1583229852@qq.com>
2019-09-03 05:00:10 -0700

    database

--- server/sql/insert.c
+++ server/sql/insert.c
@@ -2,13 +2,13 @@
 #include <stdio.h>  
 #include <mysql.h> 
 /**************************************************/
-/*名称：delete.c 
-/*描述：删除数据库内容 需修改
+/*名称：insert.c 
+/*描述：向数据库插入内容 需修改
 /*作成日期：2019-09-02
 /*作者： 王子嘉
 /***************************************************/
-////gcc -o delete -g delete.c  -I /usr/include/mysql/ -L /usr/lib/x86_64-linux-gnu/ -l mysqlclient
-void delete() {  
+////gcc -o insert -g insert.c  -I /usr/include/mysql/ -L /usr/lib/x86_64-linux-gnu/ -l mysqlclient
+void insert() {  
     MYSQL *conn_ptr;  
     int res;  
   
@@ -17,10 +17,10 @@ void delete() {
         printf("mysql_init failed\n");  
         return EXIT_FAILURE;  
     }  
-    conn_ptr = mysql_real_connect(conn_ptr, "localhost", "root", "123456", "server", 0, NULL, 0);  
+    conn_ptr = mysql_real_connect(conn_ptr, "localhost", "root", "root", "server", 0, NULL, 0);  
     if (conn_ptr) {  
-//  res = mysql_query(conn_ptr, "insert into user values(null,'Ann',5)");   
-    res = mysql_query(conn_ptr, "delete from user where name = 'Ann' and age < 10");  //更改delete语句
+        res = mysql_query(conn_ptr, "insert into user values(null,'Ann',5)");   //更改insert语句
+//      res = mysql_query(conn_ptr, "delete from user where name = 'Ann' and age < 10");  
         if (!res) {     //输出受影响的行数  
             printf("Inserted %lu rows\n",(unsigned long)mysql_affected_rows(conn_ptr));   
         }  else {       //打印出错误代码及详细信息  
--- server/src/server.cpp
+++ server/src/server.cpp
@@ -1,11 +1,12 @@
 #include "../include/data.h"
-
+#include "server.h"
 int main(){
 	init_server();
 	
-	int tid;
-	pthread_create(&tid, NULL, listen_thread_function, NULL);
-	pthread_create(&tid, NULL, send_thread_function, NULL);
+	pthread_t listen_t;
+    pthread_t send_t;
+	pthread_create(&listen_t, NULL, listen_thread_function, NULL);
+	pthread_create(&send_t, NULL, send_thread_function, NULL);
 
 	char command[1024];
 	while(1) {
